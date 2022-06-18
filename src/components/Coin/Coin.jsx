50cd275ab340b0cca80cd64dcf6fab67a0571312
Angel Psiakis <angel@mml.rocks>
2022-06-18 18:19:06 +0100

    styled components update

---
+++ src/components/Coin/Coin.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Td = styled.td`
+  border: 1px solid #cccccc;
+  width: 25vh;
+`;
+
+export default class Coin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      price: this.props.price,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+
+    const randomPercentage = 0.95 + Math.random() / 0.01;
+    this.setState((oldState) => ({
+      price: oldState.price * randomPercentage,
+    }));
+  }
+
+  render() {
+    return (
+      <tr>
+        <Td>{this.props.name}</Td>
+        <Td>{this.props.ticker}</Td>
+        <Td>${this.state.price}</Td>
+        <Td>
+          <form action="#" method="POST">
+            <button onClick={this.handleClick}>Refresh</button>
+          </form>
+        </Td>
+      </tr>
+    );
+  }
+}
+
+Coin.propTypes = {
+  name: PropTypes.string.isRequired,
+  ticker: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
