b214428644f891d613f69eb533ade97c64651d3d
Angel Psiakis <angel@mml.rocks>
2022-06-19 03:37:10 +0100

    prop drilling & componentisation

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-import AccountBalance from './components/AccountBalance/AccountBalance.jsx';
 
-import Coin from './components/Coin/Coin';
+import AccountBalance from "./components/AccountBalance/AccountBalance";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className='App-title'>Coin Exchange</h1>
-      </header>
-      <AccountBalance amount={10000} />
-      <table className='coin-table'>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Ticket</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          <Coin name="Bitcoin" ticker="BTC" price={30000} />
-          <Coin name="Ethereum" ticker="ETH" price={3000} />
-          <Coin name="Tether" ticker="USDT" price={30} />
-        </tbody>
-      </table>
-    </div>
-  );
+import React from "react";
+import CoinList from "./components/CoinList/CoinList";
+import ExchangeHeader from "./components/ExchangeHeader/ExchangeHeader";
+import styled from "styled-components";
+
+const AppDiv = styled.div`
+text-align: center;
+background-color: rgb(13, 13, 94);
+color: #cccc;
+`;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      balance: 10000,
+      coinData: [
+        { name: "Bitcoin", ticker: "BTC", price: 9999.9 },
+        { name: "Ethereum", ticker: "ETH", price: 299.99 },
+        { name: "Tether", ticker: "USDT", price: 1.0 },
+      ],
+    };
+  }
+  render() {
+    return (
+      <AppDiv>
+        <ExchangeHeader />
+        <AccountBalance amount={10000} />
+        <CoinList coinData={this.state.coinData} />
+      </AppDiv>
+    );
+  }
 }
 
 export default App;
